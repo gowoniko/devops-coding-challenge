d2ca5113d2302775bd0c0ef68978506553443d48
gowoniko <64626768+gowoniko@users.noreply.github.com>
2022-11-18 05:38:30 +0100

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://dev-devops-challenge-lb-1497694523.us-east-1.elb.amazonaws.com/?service=dev-backend'
+export const API_URL = 'http://backend-challenge.ctrl.school'
 export default API_URL
